f233a83e3d13e9a93ec14a28000d6dc14ba809f4
André Alves <andrenpalves@gmail.com>
2023-12-24 15:45:46 +0000

    Day 05 - Part 2 (cpu goes brrr)

--- day_05/main.py
+++ day_05/main.py
@@ -17,7 +17,7 @@ def part_1():
     mapped_values = []
     step = ""
 
-    file = open("day_05/input.txt", "r")
+    file = open("day_05/example.txt", "r")
     lines = [l.rstrip() for l in file]
 
     for i, line in enumerate(lines):
@@ -43,4 +43,48 @@ def part_1():
     print("Result:", result)
 
 
+def calculate_seeds(line):
+    result = []
+    values = [int(n) for n in line.split(":")[1].split()]
+
+    for i in range(0, len(values), 2):
+        seed = values[i]
+        l = values[i + 1]
+        result += list(range(seed, seed + l))
+
+    return result
+
+
+def part_2():
+    values = []
+    mapped_values = []
+    step = ""
+
+    file = open("day_05/example.txt", "r")
+    lines = [l.rstrip() for l in file]
+
+    for i, line in enumerate(lines):
+        if i == 0:
+            step = line.split(":")[0]
+            values = calculate_seeds(line)
+            mapped_values = values
+        elif ":" in line:
+            values = handle_non_mapped_values(values, mapped_values)
+            mapped_values = [None] * len(values)
+            print(values, step)
+            step = line.split(":")[0]
+        elif line == "":
+            continue
+        else:
+            range = [int(n) for n in line.split()]
+            mapped_values = map_values(values, mapped_values, range)
+
+    values = handle_non_mapped_values(values, mapped_values)
+    print(values, step)
+    print("---")
+    result = min(values)
+    print("Result:", result)
+
+
 part_1()
+part_2()
