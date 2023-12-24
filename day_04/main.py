530eb66e74a3eaa0408324babb5ad3e2d43d4f44
André Alves <andrenpalves@gmail.com>
2023-12-24 10:42:11 +0000

    Day 04 - refactorings

--- day_04/main.py
+++ day_04/main.py
@@ -1,22 +1,25 @@
+def number_of_matches(card):
+    result = 0
+    winning_numbers = card.split(":")[1].split("|")[0].split()
+    numbers = card.split(":")[1].split("|")[1].split()
+
+    for number in numbers:
+            if number in winning_numbers:
+                result +=1
+        
+    return result
+
 def main():
     result = 0
 
-    file = open("day_04/input.txt", "r")
+    file = open("day_04/example.txt", "r")
     lines = [l.rstrip() for l in file]
 
     for line in lines:
-        total = 0
-        winning_numbers = line.split(":")[1].split("|")[0].split()
-        numbers = line.split(":")[1].split("|")[1].split()
+        num_matches = number_of_matches(line)
 
-        for number in numbers:
-            if number in winning_numbers:
-                if total == 0:
-                    total += 1
-                else:
-                    total *= 2
-        
-        result +=total
+        if num_matches > 0:
+            result += 2**(num_matches-1)
     
     print("Total:", result)
 
