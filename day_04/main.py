9ac771dc3298705e9943e627642a27ac4568da26
André Alves <andrenpalves@gmail.com>
2023-12-24 11:19:33 +0000

    Day 04 - part 2

--- day_04/main.py
+++ day_04/main.py
@@ -9,7 +9,7 @@ def number_of_matches(card):
         
     return result
 
-def main():
+def part_1():
     result = 0
 
     file = open("day_04/example.txt", "r")
@@ -23,4 +23,25 @@ def main():
     
     print("Total:", result)
 
-main()
+def part_2():
+    result = 0
+    all_cards = {}
+
+    file = open("day_04/input.txt", "r")
+
+    for i,line in enumerate(file):
+        card = line.rstrip()
+        all_cards[i+1] = number_of_matches(card)
+    
+    hand = list(all_cards.keys())
+
+    for card in hand:
+        num_matches = all_cards[card]
+        
+        for n in range(card+1, card + num_matches + 1):
+            hand.append(n)
+    
+    print("Total:", len(hand))
+
+part_1()
+part_2()
